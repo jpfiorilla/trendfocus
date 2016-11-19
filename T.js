317f4f5a6342d0eb81df8051b4f65662fe9b9e2a
audiosprite <jpfiorilla@gmail.com>
2016-11-19 14:42:31 -0500

    get latest 10 tweets

---
+++ T.js
@@ -0,0 +1,11 @@
+const Twit = require('twit');
+
+const T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
+
+module.exports = T;
--- bot.js
+++ bot.js
@@ -1,21 +1,35 @@
 const googleTrends = require('google-trends-api');
 const Twit = require('twit');
+const rp = require('request-promise');
+const T = require('./T');
+const cheerio = require('cheerio');
 
-// var options = {
-//     geo: 'country code or name',
-//     date: 'yyyymm',
-//     keywords: ['some', 'list', 'of', 'keywords'],
-//     category: 'some category',
-//     timePeriod: {
-//         type: enumerated string 'hour', 'day', 'month', or 'year'
-//         value: number
-//     }
-// }
+var options = {
+    uri: 'http://www.twitter.com/realdonaldtrump',
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+};
 
-googleTrends.topRelated('trump')
-    .then(function(results){
-        console.log(results);
+// gets trump's latest 10 tweets'
+rp(options)
+    .then(function ($) {
+        // console.log($)
+        let tweets = [];
+        $('.tweet-text').each(function(i, element){
+            // console.log( i + ": " + $( this ).text() );
+            tweets.push($( this ).text());
+        })
+        console.log(tweets);
     })
-    .catch(function(err){
+    .catch(function (err) {
         console.error(err);
     });
+
+// googleTrends.topRelated('trump')
+//     .then(function(results){
+//         console.log(results);
+//     })
+//     .catch(function(err){
+//         console.error(err);
+//     });
